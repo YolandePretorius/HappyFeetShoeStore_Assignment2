39e4128ee9a4ab43099cd44f37674c5c1259c3a5
Yolande <yolande.y.kok@gmail.com>
2021-01-03 14:32:21 +1300

    Added routes

--- HappyFeetShoeStore/App_Start/RouteConfig.cs
+++ HappyFeetShoeStore/App_Start/RouteConfig.cs
@@ -13,6 +13,39 @@ namespace HappyFeetShoeStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //for create to work properly
+            routes.MapRoute(
+                 name: "ProductsCreate",
+                 url: "Products/Create",
+                 defaults: new { controller = "Products", action = "Create" }
+            );
+
+            //route for paging on Category page
+            routes.MapRoute(
+                   name: "ProductsbyCategorybyPage",
+                   url: "Products/{category}/Page{page}",
+                    defaults: new { controller = "Products", action = "Index" }
+            );
+
+            //route for paging
+            routes.MapRoute(
+                name: "ProductsbyPage",
+                 url: "Products/Page{page}",
+                defaults: new { controller = "Products", action = "Index" }
+            );
+
+            routes.MapRoute(
+                  name: "ProductsbyCategory",
+                  url: "Products/{category}",
+                  defaults: new { controller = "Products", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "ProductsIndex",
+                url: "Products",
+                defaults: new { controller = "Products", action = "Index" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
